9c9325cf3feb4cbd3d0b54710e08363b949b416a
jisenber <isenberg.jacob@yahoo.com>
2016-12-14 23:31:31 -0800

    Giving up for the night

--- lab-10-jacob/lib/challenge.js
+++ lab-10-jacob/lib/challenge.js
@@ -7,7 +7,9 @@ function challenge(array) {
   for (var i = 0; i < array.length; i++) {
     stack1.push(array[i]);
   }
-  organize(stack1, stack2, stack3);
+  while(stack3.size < 2) {
+    organize(stack1, stack2, stack3);
+  }
   console.log(stack1);
   console.log(stack2);
   console.log(stack3);
@@ -15,20 +17,62 @@ function challenge(array) {
 
 
 function organize(one, two, three) {
-  three.push(one.top.val);
-  one.pop();
-  two.push(one.top.val);
-  one.pop();
-  two.push(three.top.val);
-  three.pop();
-  three.push(one.top.val);
-  one.pop();
-  one.push(two.top.val);
-  two.pop();
-  three.push(two.top.val);
-  two.pop();
-  three.push(one.top.val);
-  one.pop();
+  if ((!three.top)&&(!two.top)) { //start em' off!
+    three.push(one.top.val);
+    one.pop();
+  }
+  else if (!three.top) {
+    if (one.top.val < two.top.val) {
+      three.push(one.top.val);
+      one.pop();
+    } else {
+      three.push(two.top.val);
+      two.pop();
+    }
+  } else if (!two.top) {
+    if (one.top.val < three.top.val) {
+      two.push(one.top.val);
+      one.pop();
+    } else {
+      two.push(three.top.val);
+      three.pop();
+    }
+  } else if (!one.top) {
+    if(two.top.val < three.top.val) {
+      one.push(two.top.val);
+      two.pop();
+    } else {
+      one.push(three.top.val);
+      three.pop();
+    }
+  } else if ((one.top.val) && (two.top.val) && (three.top.val)) {
+    if ((one.top.val > two.top.val) && (one.top.val > three.top.val)) {
+      if (two.top.val > three.top.val) {
+        three.push(two.top.val);
+        two.pop();
+      } else {
+        two.push(three.top.val);
+        three.pop();
+      }
+    } else if ((two.top.val > three.top.val) && (two.top.val > one.top.val)) {
+      if (one.top.val > three.top.val) {
+        three.push(one.top.val);
+        one.pop();
+      } else {
+        one.push(three.top.val);
+        three.pop();
+      }
+    } else if ((three.top.val > two.top.val) && (three.top.val > one.top.val)) {
+      if (two.top.val > one.top.val) {
+        one.push(two.top.val);
+        two.pop();
+      } else {
+        two.push(one.top.val);
+        one.pop();
+      }
+    }
+  }
 }
+
 
 challenge([1,2,3]);
